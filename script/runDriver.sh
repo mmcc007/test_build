d4c4ae181f76d5d2e9d719f89b9dcbcbe623cd6e
Maurice McCabe <mmcc007@gmail.com>
2018-10-04 13:31:36 -0700

    Add time outs to integration tests Test firestore_redux only

--- script/runDriver.sh
+++ script/runDriver.sh
@@ -60,4 +60,4 @@ fi
 
 # expects to find most apps at second directory level
 #find . -maxdepth 2 -type directory -exec bash -c 'runDriver "$0"' {} \;
-runDriver "./example/bloc_flutter"
+runDriver "./example/firestore_redux"
