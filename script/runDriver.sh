03199958ce584757af6d89c40efaff09e01c8117
Maurice McCabe <mmcc007@gmail.com>
2018-10-04 17:12:22 -0700

    Run all integration tests Removed some timeouts

--- script/runDriver.sh
+++ script/runDriver.sh
@@ -59,5 +59,4 @@ if isVarSet $1; then
 fi
 
 # expects to find most apps at second directory level
-#find . -maxdepth 2 -type directory -exec bash -c 'runDriver "$0"' {} \;
-runDriver "./example/simple_bloc_flutter"
+find . -maxdepth 2 -type directory -exec bash -c 'runDriver "$0"' {} \;
